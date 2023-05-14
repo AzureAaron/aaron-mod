0d1c65103fe087115f5f8bc5879c02d477596037
Aaron <51387595+AzureAaron@users.noreply.github.com>
2023-05-13 21:15:09 -0400

    A walk through the stack!
    Fixes an issue where music could get reset when exiting to the title
    screen.

---
+++ src/main/java/net/azureaaron/mod/mixins/MusicTrackerMixin.java
@@ -0,0 +1,34 @@
+package net.azureaaron.mod.mixins;
+
+import java.lang.StackWalker.StackFrame;
+import java.util.Arrays;
+import java.util.stream.Stream;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+
+import net.azureaaron.mod.Config;
+import net.minecraft.client.sound.MusicTracker;
+import net.minecraft.sound.MusicSound;
+
+@Mixin(MusicTracker.class)
+public class MusicTrackerMixin {
+	
+	private String[] findCallerMethods(Stream<StackFrame> stackFrameStream) {
+		return stackFrameStream.filter(frame -> frame.getMethodName().contains("method"))
+				.map(f -> f.getMethodName())
+				.toArray(String[]::new);
+	}
+	
+	@WrapOperation(method = "tick", at = @At(value = "INVOKE", target = "Lnet/minecraft/sound/MusicSound;shouldReplaceCurrentMusic()Z"))
+	private boolean aaronMod$dontReplaceMusicSometimes(MusicSound sound, Operation<Boolean> operation) {
+		//Walking the stack is done to ensure that we only return false when Screen#tick isn't the reason for this method invocation
+		String[] callerMethods = StackWalker.getInstance().walk(this::findCallerMethods);
+		boolean calledScreenTick = Arrays.stream(callerMethods).anyMatch("method_25393"::equals);
+		
+		return Config.stopSoundsOnWorldChange && !calledScreenTick ? false : operation.call(sound);
+	}
+}
