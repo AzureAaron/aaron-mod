85cdb0468550180631fd0392b4dbca6322551c3d
Aaron <51387595+AzureAaron@users.noreply.github.com>
2023-07-04 00:32:32 -0400

    Make inDungeonBossRoom volatile

--- src/main/java/net/azureaaron/mod/util/Cache.java
+++ src/main/java/net/azureaaron/mod/util/Cache.java
@@ -27,7 +27,7 @@ public class Cache {
 	public static long lastTwoHundredSeventyScore = 0L;
 	public static long lastThreeHundredScore = 0L;	
 	public static boolean inM7Phase5 = false;
-	public static boolean inDungeonBossRoom = false;
+	public static volatile boolean inDungeonBossRoom = false;
 	public static int currentScore = 0;
 	
 	public static String powerDragonUuid;
