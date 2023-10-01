0e6b2ed8950be62fd4e6c905e427ecad196485f7
Aaron <51387595+AzureAaron@users.noreply.github.com>
2023-10-01 03:14:59 -0400

    Add shortcut command to config

--- src/main/java/net/azureaaron/mod/commands/ModScreenCommand.java
+++ src/main/java/net/azureaaron/mod/commands/ModScreenCommand.java
@@ -2,27 +2,59 @@ package net.azureaaron.mod.commands;
 
 import static net.fabricmc.fabric.api.client.command.v2.ClientCommandManager.literal;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 import com.mojang.brigadier.Command;
 import com.mojang.brigadier.CommandDispatcher;
 
+import net.azureaaron.mod.Config;
 import net.azureaaron.mod.screens.ModScreen;
 import net.fabricmc.fabric.api.client.command.v2.FabricClientCommandSource;
 import net.minecraft.client.MinecraftClient;
 
 public class ModScreenCommand {
+	private static final long ONE_TICK = 200L;
+	
 	public static void register(CommandDispatcher<FabricClientCommandSource> dispatcher) {
 		dispatcher.register(literal("aaronsmod")
-				.executes(context -> handleCommand(context.getSource())));
+				.executes(context -> handleCommand(context.getSource()))
+				.then(literal("config")
+						.executes(context -> handleOpenConfig(context.getSource())))
+				.then(literal("options")
+						.executes(context -> handleOpenConfig(context.getSource()))));
 		
 		dispatcher.register(literal("aaronmod")
-				.executes(context -> handleCommand(context.getSource())));
+				.executes(context -> handleCommand(context.getSource()))
+				.then(literal("config")
+						.executes(context -> handleOpenConfig(context.getSource())))
+				.then(literal("options")
+						.executes(context -> handleOpenConfig(context.getSource()))));
 	}
 	
-	private static int handleCommand(FabricClientCommandSource context) {
-		MinecraftClient client = context.getClient();
+	private static int handleCommand(FabricClientCommandSource source) {
+		MinecraftClient client = source.getClient();
+		
 		client.send(() -> {
 			client.setScreen(new ModScreen(null));
 		});
+				
+		return Command.SINGLE_SUCCESS;
+	}
+	
+	private static int handleOpenConfig(FabricClientCommandSource source) {
+		MinecraftClient client = source.getClient();
+		TimerTask timedTask = new TimerTask() {
+			@Override
+			public void run() {
+				client.send(() -> {
+					client.setScreen(Config.createGui(null));
+				});
+			}
+		};
+		
+		new Timer().schedule(timedTask, ONE_TICK);
+		
 		return Command.SINGLE_SUCCESS;
 	}
 }
