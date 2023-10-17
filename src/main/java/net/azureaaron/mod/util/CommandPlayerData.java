0beb741e1422cf6f2b9f67cbf6a425e847b8e53a
Aaron <51387595+AzureAaron@users.noreply.github.com>
2023-10-17 01:11:56 -0400

    Codec Test

--- src/main/java/net/azureaaron/mod/util/CommandPlayerData.java
+++ src/main/java/net/azureaaron/mod/util/CommandPlayerData.java
@@ -1,5 +1,8 @@
 package net.azureaaron.mod.util;
 
+import com.mojang.serialization.Codec;
+import com.mojang.serialization.codecs.RecordCodecBuilder;
+
 /**
  * Record which holds a player's name and uuid, similar to a {@link com.mojang.authlib.GameProfile GameProfile}.
  * This is used for commands that take in a player argument.
@@ -7,4 +10,8 @@ package net.azureaaron.mod.util;
  * @author Aaron
  */
 public record CommandPlayerData(String name, String uuid) {
+	public static final Codec<CommandPlayerData> CODEC = RecordCodecBuilder.create(instance -> instance.group(
+			Codec.STRING.fieldOf("name").forGetter(CommandPlayerData::name),
+			Codec.STRING.fieldOf("uuid").forGetter(CommandPlayerData::uuid)
+			).apply(instance, CommandPlayerData::new));
 }
