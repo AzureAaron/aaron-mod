5db94724b02cc23b0439f584f67ddea5e9d1defd
Aaron <51387595+AzureAaron@users.noreply.github.com>
2023-11-18 01:15:01 -0500

    Refactor ModMenu entrypoint

--- src/main/java/net/azureaaron/mod/ModMenu.java
+++ src/main/java/net/azureaaron/mod/ModMenu.java
@@ -6,8 +6,6 @@ import com.terraformersmc.modmenu.api.ModMenuApi;
 public class ModMenu implements ModMenuApi {
 	@Override
     public ConfigScreenFactory<?> getModConfigScreenFactory() {
-		return (parent) -> {
-			return Config.createGui(parent);
-		};
+		return Config::createGui;
 	}
 }
