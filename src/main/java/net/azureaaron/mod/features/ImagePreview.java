b082509fdec48b5cbc89822fb93eb770b85b72d9
Aaron <51387595+AzureAaron@users.noreply.github.com>
2024-04-26 13:34:47 -0400

    Fix image preview rendering behind the chat

--- src/main/java/net/azureaaron/mod/features/ImagePreview.java
+++ src/main/java/net/azureaaron/mod/features/ImagePreview.java
@@ -32,7 +32,7 @@ import net.minecraft.util.Identifier;
 public class ImagePreview {
 	private static final Pattern IMAGE_URL_PATTERN = Pattern.compile("(https?:\\/\\/.*\\.(?:png|jpg|jpeg|gif)\\.?(?:\\?.+)?)", Pattern.CASE_INSENSITIVE);
 	private static final ImmutableSet<String> EXPECTED_CONTENT_TYPES = ImmutableSet.of("image/png", "image/jpeg");
-	
+
 	//The actual image caches, we retain a separate set of urls to avoid attempting to cache the same image twice which'd cause a memory leak
 	private static final Object2ObjectOpenHashMap<String, CachedImage> IMAGE_CACHE = new Object2ObjectOpenHashMap<>();
 	private static final ObjectOpenHashSet<String> IMAGE_URLS_CACHED = new ObjectOpenHashSet<>();
@@ -40,29 +40,31 @@ public class ImagePreview {
 	public static void init() {
 		ReceiveChatMessageEvent.EVENT.register(ImagePreview::inspectMessageForImageLinks);
 		ScreenEvents.AFTER_INIT.register((client, screen, scaledWidth, scaledHeight) -> {
-			ScreenEvents.afterRender(screen).register((screen1, context, mouseX, mouseY, delta) -> {
-				afterScreenRendered(screen1, context, mouseX, mouseY);
-			});
+			if (screen instanceof ChatScreen && AaronModConfigManager.get().imagePreview) {
+				ScreenEvents.afterRender(screen).register((screen1, context, mouseX, mouseY, delta) -> {
+					afterScreenRendered(screen1, context, mouseX, mouseY);
+				});
+			}
 		});
 	}
-	
+
 	private static void inspectMessageForImageLinks(Text text, boolean overlay, boolean cancelled) {
 		if (AaronModConfigManager.get().imagePreview && !overlay && !cancelled) {
 			ObjectOpenHashSet<String> foundImages = new ObjectOpenHashSet<>();
 			Text deconstructedText = TextTransformer.deconstructAllComponents(text); 
 			List<Text> components = deconstructedText.getSiblings();
-			
+
 			for (Text currentComponent : components) {
 				Style currentStyle = currentComponent.getStyle();
 				ClickEvent clickEvent = currentStyle.getClickEvent();
-				
+
 				if (clickEvent != null && clickEvent.getAction() == ClickEvent.Action.OPEN_URL) {
 					String url = fixupLink(clickEvent.getValue());
-					
+
 					if (IMAGE_URL_PATTERN.matcher(url).matches()) foundImages.add(url);
 				}
 			}
-					
+
 			for (String imageUrl: foundImages) {
 				if (!IMAGE_URLS_CACHED.contains(imageUrl)) {
 					cacheImage(imageUrl);
@@ -70,61 +72,60 @@ public class ImagePreview {
 			}
 		}
 	}
-	
+
 	private static void cacheImage(String url) {
 		CompletableFuture.runAsync(() -> {
 			MinecraftClient client = MinecraftClient.getInstance();
 			URI uri = URI.create(url);
-			
+
 			if (isAllowedImageHost(uri.getHost())) {
 				IMAGE_URLS_CACHED.add(url);
-				
+
 				try {
 					InputStream inputStream = Http.sendGenericH2Request(uri, EXPECTED_CONTENT_TYPES);
 					NativeImage image = NativeImage.read(inputStream);
 					Identifier texture = client.getTextureManager().registerDynamicTexture("image_preview", new NativeImageBackedTexture(image));
-					
+
 					IMAGE_CACHE.put(url, new CachedImage(System.currentTimeMillis(), texture, image.getWidth(), image.getHeight()));
 				} catch (Exception e) {
 					Main.LOGGER.error("[Aaron's Mod Image Preivew] Failed to cache image! URL: {}", url, e);
-					
+
 					IMAGE_URLS_CACHED.remove(url);
 				}
 			}
 		});
 	}
-	
+
 	private static void afterScreenRendered(Screen screen, DrawContext context, int mouseX, int mouseY) {
-		if (screen instanceof ChatScreen && AaronModConfigManager.get().imagePreview) {
-			MinecraftClient client = MinecraftClient.getInstance();
-			Style style = client.inGameHud.getChatHud().getTextStyleAt(mouseX, mouseY);
-			
-			if (style != null && style.getClickEvent() != null) {
-				ClickEvent clickEvent = style.getClickEvent();
-				
-				if (clickEvent.getAction() == ClickEvent.Action.OPEN_URL) {
-					CachedImage image = ImagePreview.IMAGE_CACHE.getOrDefault(fixupLink(clickEvent.getValue()), null);
-					
-					if (image != null) {
-						MatrixStack matrices = context.getMatrices();
-						int width = image.width();
-						int height = image.height();
-						
-						//Reasonably scale the image so it doesn't go off screen and isn't too large or too small
-						float scale = Math.min(1f, Math.min(getPrevHeightDiv() / height, getPrevWidthDiv() / width));
-						
-						matrices.push();
-						matrices.scale(scale, scale, 200f);
-						
-						context.drawTexture(image.texture(), 0, 0, 0, 0, width, height, width, height);
-						
-						matrices.pop();
-					}
+		MinecraftClient client = MinecraftClient.getInstance();
+		Style style = client.inGameHud.getChatHud().getTextStyleAt(mouseX, mouseY);
+
+		if (style != null && style.getClickEvent() != null) {
+			ClickEvent clickEvent = style.getClickEvent();
+
+			if (clickEvent.getAction() == ClickEvent.Action.OPEN_URL) {
+				CachedImage image = ImagePreview.IMAGE_CACHE.getOrDefault(fixupLink(clickEvent.getValue()), null);
+
+				if (image != null) {
+					MatrixStack matrices = context.getMatrices();
+					int width = image.width();
+					int height = image.height();
+
+					//Reasonably scale the image so it doesn't go off screen and isn't too large or too small
+					float scale = Math.min(1f, Math.min(getPrevHeightDiv() / height, getPrevWidthDiv() / width));
+
+					matrices.push();
+					matrices.scale(scale, scale, 1f); //The 1f is needed otherwise it'll render behind the chat (the chat's z is scaled by 1 too)
+					matrices.translate(0f, 0f, 200f);
+
+					context.drawTexture(image.texture(), 0, 0, 0, 0, width, height, width, height);
+
+					matrices.pop();
 				}
 			}
 		}
 	}
-	
+
 	/**
 	 * Clears the image cache and frees memory associated with the images
 	 */
@@ -132,31 +133,31 @@ public class ImagePreview {
 		for (Map.Entry<String, CachedImage> entry : IMAGE_CACHE.entrySet()) {
 			client.getTextureManager().destroyTexture(entry.getValue().texture());
 		}
-		
+
 		IMAGE_CACHE.clear();
 		IMAGE_URLS_CACHED.clear();
 	}
-	
+
 	private static boolean isAllowedImageHost(String host) {
 		return host.equals("cdn.discordapp.com") || host.equals("media.discordapp.net") || host.equals("i.imgur.com");
 	}
-	
+
 	/**
 	 * Converts imgur.com to i.imgur.com
 	 */
 	private static String fixupLink(String url) {
 		if (url.startsWith("https://imgur.com")) return url.replace("https://imgur.com", "https://i.imgur.com");
-			
+
 		return url;
 	}
-	
+
 	private static float getPrevHeightDiv() {
 		return (6.75f * 15f) * AaronModConfigManager.get().imagePreviewScale;
 	}
-	
+
 	private static float getPrevWidthDiv() {
 		return (12f * 15f) * AaronModConfigManager.get().imagePreviewScale;
 	}
-	
+
 	private static record CachedImage(long creationTime, Identifier texture, int width, int height) {}
 }
