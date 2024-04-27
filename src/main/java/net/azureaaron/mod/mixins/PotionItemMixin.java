8b4302c29160a39d9500dbb59561c2abcf255176
Aaron <51387595+AzureAaron@users.noreply.github.com>
2024-04-26 20:29:57 -0400

    Fix water bottles having a glint

--- src/main/java/net/azureaaron/mod/mixins/PotionItemMixin.java
+++ src/main/java/net/azureaaron/mod/mixins/PotionItemMixin.java
@@ -4,6 +4,7 @@ import org.spongepowered.asm.mixin.Mixin;
 
 import net.azureaaron.mod.config.AaronModConfigManager;
 import net.minecraft.component.DataComponentTypes;
+import net.minecraft.component.type.PotionContentsComponent;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.PotionItem;
@@ -17,6 +18,6 @@ public class PotionItemMixin extends Item {
 
 	@Override
 	public boolean hasGlint(ItemStack stack) {
-		return AaronModConfigManager.get().shinyPotions && (super.hasGlint(stack) || stack.contains(DataComponentTypes.POTION_CONTENTS));
+		return AaronModConfigManager.get().shinyPotions && (super.hasGlint(stack) || stack.getOrDefault(DataComponentTypes.POTION_CONTENTS, PotionContentsComponent.DEFAULT).hasEffects());
 	}
 }
