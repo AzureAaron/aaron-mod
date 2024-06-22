3b0868741a053c73472497c8232436297b8c121f
Aaron <51387595+AzureAaron@users.noreply.github.com>
2024-06-22 12:06:25 -0400

    Chroma Text Interop API

--- src/main/java/net/azureaaron/mod/features/ChromaText.java
+++ src/main/java/net/azureaaron/mod/features/ChromaText.java
@@ -3,12 +3,34 @@ package net.azureaaron.mod.features;
 import net.azureaaron.mod.Main;
 import net.fabricmc.fabric.api.resource.ResourceManagerHelper;
 import net.fabricmc.fabric.api.resource.ResourcePackActivationType;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.util.Identifier;
 
 public class ChromaText {
+	private static final MinecraftClient CLIENT = MinecraftClient.getInstance();
 	public static final Identifier ID = Identifier.of(Main.NAMESPACE, "chroma_text");
 
 	public static void init() {
 		ResourceManagerHelper.registerBuiltinResourcePack(ID, Main.MOD_CONTAINER, ResourcePackActivationType.DEFAULT_ENABLED);
+	}
+
+	/**
+	 * Intended for compatibility and interop purposes. Note that this does not guarantee that there isn't another pack enabled
+	 * on top which overrides the necessary shaders required for it to work.
+	 * 
+	 * For accurate results, this method should only be called after the client has loaded resource packs.
+	 * 
+	 * @return whether the chroma text pack is enabled
+	 */
+	public static boolean chromaColourAvailable() {
+		try {
+			for (String id : CLIENT.getResourcePackManager().getEnabledIds()) {
+				if (id.equals(ID.toString())) {
+					return true;
+				}
+			}
+		} catch (Throwable ignored) {}
+
+		return false;
 	}
 }
