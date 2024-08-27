35bf5bc2d34c97336c01950aa596100ac2d4969f
Aaron <51387595+AzureAaron@users.noreply.github.com>
2024-08-27 16:17:42 -0400

    Refactor ParticleManager Mixin with @Cancellable

--- src/main/java/net/azureaaron/mod/mixins/ParticleManagerMixin.java
+++ src/main/java/net/azureaaron/mod/mixins/ParticleManagerMixin.java
@@ -4,12 +4,14 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import com.llamalad7.mixinextras.sugar.Cancellable;
 import com.llamalad7.mixinextras.sugar.Local;
 
 import net.azureaaron.mod.Particles;
@@ -25,11 +27,20 @@ import net.minecraft.util.Identifier;
 @Mixin(ParticleManager.class)
 public class ParticleManagerMixin {
 
-	@Inject(method = "addParticle(Lnet/minecraft/particle/ParticleEffect;DDDDDD)Lnet/minecraft/client/particle/Particle;", at = @At("HEAD"), cancellable = true) 
-	private void aaronMod$modifyParticles(CallbackInfoReturnable<Particle> cir, @Local(argsOnly = true) ParticleEffect parameters) {
-		Identifier particleId = Registries.PARTICLE_TYPE.getId(parameters.getType());
+	@ModifyVariable(method = "addParticle(Lnet/minecraft/particle/ParticleEffect;DDDDDD)Lnet/minecraft/client/particle/Particle;", at = @At("STORE"))
+	private Particle aaronMod$modifyAndScaleParticles(Particle original, @Local(argsOnly = true) ParticleEffect parameters, @Cancellable CallbackInfoReturnable<Particle> cir) {
+		if (original != null) {
+			Identifier particleId = Registries.PARTICLE_TYPE.getId(parameters.getType());
 
-		if (AaronModConfigManager.get().particles.getOrDefault(particleId, State.FULL) == State.NONE) cir.setReturnValue(null);
+			switch (AaronModConfigManager.get().particles.getOrDefault(particleId, State.FULL)) {
+				case State.NONE -> cir.setReturnValue(null);
+				case State.FULL -> {
+					return scaleParticle(original, AaronModConfigManager.get().particleScaling.getOrDefault(particleId, 1f));
+				}
+			}
+		}
+
+		return original;
 	}
 
 	@Inject(method = "addBlockBreakParticles", at = @At("HEAD"), cancellable = true)
@@ -43,13 +54,6 @@ public class ParticleManagerMixin {
 	}
 
 	//Particle Scale stuff
-
-	@Inject(method = "addParticle(Lnet/minecraft/particle/ParticleEffect;DDDDDD)Lnet/minecraft/client/particle/Particle;", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/particle/ParticleManager;addParticle(Lnet/minecraft/client/particle/Particle;)V", shift = At.Shift.BEFORE))
-	private void aaronMod$modifyParticleScale(CallbackInfoReturnable<Particle> cir, @Local(argsOnly = true) ParticleEffect parameters, @Local Particle particle) {
-		Identifier particleId = Registries.PARTICLE_TYPE.getId(parameters.getType());
-
-		scaleParticle(particle, AaronModConfigManager.get().particleScaling.getOrDefault(particleId, 1f));
-	}
 
 	@ModifyExpressionValue(method = "method_34020", at = @At(value = "NEW", target = "Lnet/minecraft/client/particle/BlockDustParticle;"))
 	private BlockDustParticle aaronMod$changeBlockBreakScale(BlockDustParticle original) {
