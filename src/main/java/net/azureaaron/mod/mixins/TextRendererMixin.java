f885ec9b0bb06eb7c3d2c74f385c69fa5492bb58
Aaron <51387595+AzureAaron@users.noreply.github.com>
2024-10-31 19:09:48 -0400

    Use different injection for Text Replacer
    Should improve compatibility with txt rendering mods

--- src/main/java/net/azureaaron/mod/mixins/TextRendererMixin.java
+++ src/main/java/net/azureaaron/mod/mixins/TextRendererMixin.java
@@ -2,13 +2,7 @@ package net.azureaaron.mod.mixins;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-
-import com.llamalad7.mixinextras.sugar.Local;
-import com.llamalad7.mixinextras.sugar.Share;
-import com.llamalad7.mixinextras.sugar.ref.LocalRef;
 
 import net.azureaaron.mod.config.AaronModConfigManager;
 import net.azureaaron.mod.features.TextReplacer;
@@ -18,13 +12,8 @@ import net.minecraft.text.OrderedText;
 @Mixin(TextRenderer.class)
 public class TextRendererMixin {
 
-	@Inject(method = "drawInternal(Lnet/minecraft/text/OrderedText;FFIZLorg/joml/Matrix4f;Lnet/minecraft/client/render/VertexConsumerProvider;Lnet/minecraft/client/font/TextRenderer$TextLayerType;IIZ)I", at = @At("HEAD"))
-	private void aaronMod$visuallyReplaceOrderedText(CallbackInfoReturnable<Integer> cir, @Local(argsOnly = true) OrderedText text, @Share("newText") LocalRef<OrderedText> newText) {
-		if (AaronModConfigManager.get().visualTextReplacer) newText.set(TextReplacer.visuallyReplaceText(text));
-	}
-
-	@ModifyVariable(method = "drawInternal(Lnet/minecraft/text/OrderedText;FFIZLorg/joml/Matrix4f;Lnet/minecraft/client/render/VertexConsumerProvider;Lnet/minecraft/client/font/TextRenderer$TextLayerType;IIZ)I", at = @At("LOAD"))
-	private OrderedText aaronMod$actuallyChangeTheText(OrderedText text, @Share("newText") LocalRef<OrderedText> newText) {
-		return AaronModConfigManager.get().visualTextReplacer ? newText.get() : text;
+	@ModifyVariable(method = "drawInternal(Lnet/minecraft/text/OrderedText;FFIZLorg/joml/Matrix4f;Lnet/minecraft/client/render/VertexConsumerProvider;Lnet/minecraft/client/font/TextRenderer$TextLayerType;IIZ)I", at = @At("HEAD"), argsOnly = true)
+	private OrderedText aaronMod$visuallyReplaceOrderedText(OrderedText text) {
+		return AaronModConfigManager.get().visualTextReplacer ? TextReplacer.visuallyReplaceText(text) : text;
 	}
 }
