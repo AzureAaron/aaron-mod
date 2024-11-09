154c0d1a4bff930234a9bef3389044d6cfae73b2
Aaron <51387595+AzureAaron@users.noreply.github.com>
2024-11-09 15:43:39 -0500

    Fix threading issue with dragon spawn notifications
    Rarely, not running this code on the render thread can trigger a CME in
    the SoundSystem.

--- src/main/java/net/azureaaron/mod/features/DragonNotifications.java
+++ src/main/java/net/azureaaron/mod/features/DragonNotifications.java
@@ -1,5 +1,7 @@
 package net.azureaaron.mod.features;
 
+import com.mojang.blaze3d.systems.RenderSystem;
+
 import net.azureaaron.mod.config.AaronModConfigManager;
 import net.azureaaron.mod.utils.Constants;
 import net.minecraft.client.MinecraftClient;
@@ -14,19 +16,21 @@ public class DragonNotifications {
 	 */
 	protected static void notifySpawn(Dragons dragon) {
 		if (AaronModConfigManager.get().m7DragonSpawnNotifications) {
-			MinecraftClient client = MinecraftClient.getInstance();
+			RenderSystem.recordRenderCall(() -> {
+				MinecraftClient client = MinecraftClient.getInstance();
 
-			if (client.player == null) return; //Shouldn't be the case ever
+				if (client.player == null) return; //Shouldn't be the case ever
 
-			InGameHud hud = client.inGameHud;
-			Text text = Text.literal(dragon.name).withColor(dragon.colour);
+				InGameHud hud = client.inGameHud;
+				Text text = Text.literal(dragon.name).withColor(dragon.colour);
 
-			hud.clearTitle();
-			hud.setDefaultTitleFade();
-			hud.setTitle(text);
+				hud.clearTitle();
+				hud.setDefaultTitleFade();
+				hud.setTitle(text);
 
-			client.player.sendMessage(Constants.PREFIX.get().append(Text.literal("The ")).append(text).append(Text.literal(" dragon will spawn soon!")), false);
-			client.player.playSound(SoundEvents.ENTITY_EXPERIENCE_ORB_PICKUP, 100f, 0.1f);
+				client.player.sendMessage(Constants.PREFIX.get().append(Text.literal("The ")).append(text).append(Text.literal(" dragon will spawn soon!")), false);
+				client.player.playSound(SoundEvents.ENTITY_EXPERIENCE_ORB_PICKUP, 100f, 0.1f);
+			});
 		}
 	}
 }
