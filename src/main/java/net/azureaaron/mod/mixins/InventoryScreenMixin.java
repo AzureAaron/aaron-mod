abbf21f5e0bfba51f42bdfdd0ae199d6c76d6d98
Aaron <51387595+AzureAaron@users.noreply.github.com>
2025-01-17 17:42:25 -0500

    Item searching fixes

--- src/main/java/net/azureaaron/mod/mixins/InventoryScreenMixin.java
+++ src/main/java/net/azureaaron/mod/mixins/InventoryScreenMixin.java
@@ -2,6 +2,7 @@ package net.azureaaron.mod.mixins;
 
 import java.util.List;
 
+import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
@@ -14,31 +15,61 @@ import net.minecraft.client.gui.screen.ingame.InventoryScreen;
 import net.minecraft.client.gui.screen.ingame.RecipeBookScreen;
 import net.minecraft.client.gui.screen.recipebook.RecipeBookProvider;
 import net.minecraft.client.gui.screen.recipebook.RecipeBookWidget;
+import net.minecraft.client.search.SearchManager;
 import net.minecraft.component.DataComponentTypes;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemGroups;
 import net.minecraft.item.ItemStack;
+import net.minecraft.registry.RegistryWrapper.WrapperLookup;
+import net.minecraft.resource.featuretoggle.FeatureSet;
 import net.minecraft.screen.PlayerScreenHandler;
 import net.minecraft.text.Text;
 import net.minecraft.util.Formatting;
 
 @Mixin(InventoryScreen.class)
 public abstract class InventoryScreenMixin extends RecipeBookScreen<PlayerScreenHandler> implements RecipeBookProvider {
+	@Unique
+	private static final MinecraftClient CLIENT = MinecraftClient.getInstance();
 
-	public InventoryScreenMixin(PlayerScreenHandler handler, RecipeBookWidget<?> recipeBook, PlayerInventory inventory, Text title) {
+	protected InventoryScreenMixin(PlayerScreenHandler handler, RecipeBookWidget<?> recipeBook, PlayerInventory inventory, Text title) {
 		super(handler, recipeBook, inventory, title);
 	}
 
 	@Inject(method = "<init>", at = @At("TAIL"))
 	private void aaronMod$initDisplayGroups(PlayerEntity player, CallbackInfo ci) {
-		if (!MinecraftClient.getInstance().interactionManager.hasCreativeInventory() && AaronModConfigManager.get().showItemGroupsOutsideOfCreative) ItemGroups.updateDisplayContext(MinecraftClient.getInstance().player.networkHandler.getEnabledFeatures(), shouldShowOperatorTab(player), player.getWorld().getRegistryManager());
+		if (!CLIENT.interactionManager.hasCreativeInventory() && AaronModConfigManager.get().showItemGroupsOutsideOfCreative) {
+			updateDisplayGroups(CLIENT.getNetworkHandler().getSearchManager(), CLIENT.getNetworkHandler().getEnabledFeatures(), shouldShowOperatorTab(player), player.getRegistryManager());
+		}
+	}
+
+	@Inject(method = "handledScreenTick", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/screen/ingame/RecipeBookScreen;handledScreenTick()V", shift = At.Shift.AFTER))
+	private void aaronMod$updateDisplayGroups(CallbackInfo ci) {
+		if (!CLIENT.interactionManager.hasCreativeInventory() && CLIENT.player != null && AaronModConfigManager.get().showItemGroupsOutsideOfCreative) {
+			updateDisplayGroups(CLIENT.getNetworkHandler().getSearchManager(), CLIENT.getNetworkHandler().getEnabledFeatures(), shouldShowOperatorTab(CLIENT.player), CLIENT.player.getRegistryManager());
+		}
 	}
 
 	@Unique
 	private boolean shouldShowOperatorTab(PlayerEntity player) {
-		return player.isCreativeLevelTwoOp() && MinecraftClient.getInstance().options.getOperatorItemsTab().getValue().booleanValue();
+		return player.isCreativeLevelTwoOp() && CLIENT.options.getOperatorItemsTab().getValue();
+	}
+
+	@Unique
+	private boolean updateDisplayGroups(@Nullable SearchManager searchManager, FeatureSet enabledFeatures, boolean showOperatorTab, WrapperLookup registries) {
+		if (!ItemGroups.updateDisplayContext(enabledFeatures, showOperatorTab, registries)) {
+			return false;
+		} else {
+			if (searchManager != null) {
+				List<ItemStack> list = List.copyOf(ItemGroups.getSearchGroup().getDisplayStacks());
+
+				searchManager.addItemTooltipReloader(registries, list);
+				searchManager.addItemTagReloader(list);
+			}
+
+			return true;
+		}
 	}
 
 	@Override
