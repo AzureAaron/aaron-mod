7c90d56586910d5fd1feda2072dfafa47f2344a4
Aaron <51387595+AzureAaron@users.noreply.github.com>
2025-02-11 01:34:09 -0500

    Fix z formatting code overwriting text shadow colour

--- src/main/java/net/azureaaron/mod/mixins/TextVisitFactoryMixin.java
+++ src/main/java/net/azureaaron/mod/mixins/TextVisitFactoryMixin.java
@@ -9,18 +9,22 @@ import com.llamalad7.mixinextras.sugar.Local;
 import com.llamalad7.mixinextras.sugar.ref.LocalRef;
 
 import net.minecraft.text.Style;
+import net.minecraft.text.TextColor;
 import net.minecraft.text.TextVisitFactory;
 
 @Mixin(TextVisitFactory.class)
 public class TextVisitFactoryMixin {
 	@Unique
-	private static final Style CHROMA_BASE = Style.EMPTY.withColor(0xAA5500);
+	private static final TextColor CHROMA = TextColor.fromRgb(0xAA5500);
 
 	@ModifyExpressionValue(method = "visitFormatted(Ljava/lang/String;ILnet/minecraft/text/Style;Lnet/minecraft/text/Style;Lnet/minecraft/text/CharacterVisitor;)Z", at = @At(value = "INVOKE", target = "Ljava/lang/String;charAt(I)C", ordinal = 1, remap = false))
 	private static char aaronMod$zFormattingCode(char code, @Local(ordinal = 2) LocalRef<Style> style) {
-		Style currentStyle = style.get();
+		if (Character.toLowerCase(code) == 'z') {
+			Style currentStyle = style.get();
+			Style newStyle = new Style(CHROMA, currentStyle.getShadowColor(), false, false, false, false, false, currentStyle.getClickEvent(), currentStyle.getHoverEvent(), currentStyle.getInsertion(), currentStyle.getFont());
 
-		if (Character.toLowerCase(code) == 'z') style.set(CHROMA_BASE.withClickEvent(currentStyle.getClickEvent()).withHoverEvent(currentStyle.getHoverEvent()).withInsertion(currentStyle.getInsertion()).withFont(currentStyle.getFont()));
+			style.set(newStyle);
+		}
 
 		return code;
 	}
