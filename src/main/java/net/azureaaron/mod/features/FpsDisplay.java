b729e0669f9cf76b270211790b5d9180992827c2
Aaron <51387595+AzureAaron@users.noreply.github.com>
2025-02-16 03:17:51 -0500

    Adopt new Fabric HUD Rendering API

--- src/main/java/net/azureaaron/mod/features/FpsDisplay.java
+++ src/main/java/net/azureaaron/mod/features/FpsDisplay.java
@@ -1,16 +1,22 @@
 package net.azureaaron.mod.features;
 
+import net.azureaaron.mod.Main;
 import net.azureaaron.mod.annotations.Init;
 import net.azureaaron.mod.config.AaronModConfigManager;
 import net.azureaaron.mod.utils.render.hud.TextHudElement;
+import net.fabricmc.fabric.api.client.rendering.v1.HudLayerRegistrationCallback;
+import net.fabricmc.fabric.api.client.rendering.v1.IdentifiedLayer;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.DrawContext;
+import net.minecraft.client.render.RenderTickCounter;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.text.Text;
 import net.minecraft.util.Colors;
+import net.minecraft.util.Identifier;
 
 public class FpsDisplay {
 	private static final MinecraftClient CLIENT = MinecraftClient.getInstance();
+	private static final Identifier ID = Identifier.of(Main.NAMESPACE, "fps");
 	@SuppressWarnings("unused")
 	private static final TextHudElement HUD_ELEMENT = new TextHudElement(
 			Text.of("100 fps"),
@@ -24,9 +30,11 @@ public class FpsDisplay {
 			2);
 
 	@Init
-	public static void init() {}
+	public static void init() {
+		HudLayerRegistrationCallback.EVENT.register(d -> d.attachLayerAfter(IdentifiedLayer.STATUS_EFFECTS, ID, FpsDisplay::render));
+	}
 
-	public static void render(DrawContext context) {
+	private static void render(DrawContext context, RenderTickCounter tickCounter) {
 		if (!CLIENT.getDebugHud().shouldShowDebugHud()) {
 			int x = AaronModConfigManager.get().fpsDisplayX;
 			int y = AaronModConfigManager.get().fpsDisplayY;
--- src/main/java/net/azureaaron/mod/mixins/InGameHudMixin.java
+++ src/main/java/net/azureaaron/mod/mixins/InGameHudMixin.java
@@ -4,17 +4,13 @@ import java.util.function.Function;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.Redirect;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
-import com.llamalad7.mixinextras.sugar.Local;
 
 import net.azureaaron.mod.config.AaronModConfigManager;
-import net.azureaaron.mod.features.FpsDisplay;
 import net.azureaaron.mod.features.TextReplacer;
 import net.minecraft.client.font.TextRenderer;
 import net.minecraft.client.gui.DrawContext;
@@ -39,14 +35,6 @@ public class InGameHudMixin {
     @WrapOperation(method = "renderScoreboardSidebar(Lnet/minecraft/client/gui/DrawContext;Lnet/minecraft/scoreboard/ScoreboardObjective;)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/DrawContext;drawText(Lnet/minecraft/client/font/TextRenderer;Lnet/minecraft/text/Text;IIIZ)I"))
     private int aaronMod$shadowEntryTitleAndScoreText(DrawContext context, TextRenderer textRenderer, Text text, int x, int y, int colour, boolean shadow, Operation<Integer> operation) {
     	return AaronModConfigManager.get().shadowedScoreboard ? context.drawTextWithShadow(textRenderer, text, x, y, colour) : operation.call(context, textRenderer, text, x, y, colour, shadow);
-    }
-
-    /**
-     * The first thing thats rendered to the HUD to ensure proper layering.
-     */
-    @Inject(method = "renderMiscOverlays", at = @At("HEAD"))
-    public void aaronMod$fpsDisplay(CallbackInfo ci, @Local(argsOnly = true) DrawContext context) {
-    	if (AaronModConfigManager.get().fpsDisplay) FpsDisplay.render(context);
     }
 
     @WrapOperation(method = "renderHeldItemTooltip", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/font/TextRenderer;getWidth(Lnet/minecraft/text/StringVisitable;)I"))
