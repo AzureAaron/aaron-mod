8104c5d4bb9edba840855770bcc0ca8242617dfb
Aaron <51387595+AzureAaron@users.noreply.github.com>
2025-02-16 22:54:38 -0500

    Improve Image Preview thread safety
    Previously it was falling into the dangerous trap that is Mojang's
    system for handling off-thread GL operations.

--- src/main/java/net/azureaaron/mod/features/ImagePreview.java
+++ src/main/java/net/azureaaron/mod/features/ImagePreview.java
@@ -2,22 +2,24 @@ package net.azureaaron.mod.features;
 
 import java.io.InputStream;
 import java.net.URI;
-import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.regex.Pattern;
 
-import com.google.common.collect.ImmutableSet;
+import org.slf4j.Logger;
 
-import it.unimi.dsi.fastutil.objects.Object2ObjectOpenHashMap;
+import com.google.common.collect.ImmutableSet;
+import com.mojang.logging.LogUtils;
+
 import it.unimi.dsi.fastutil.objects.ObjectOpenHashSet;
 import net.azureaaron.mod.Main;
 import net.azureaaron.mod.annotations.Init;
 import net.azureaaron.mod.config.AaronModConfigManager;
 import net.azureaaron.mod.events.ReceiveChatMessageEvent;
 import net.azureaaron.mod.utils.Http;
-import net.azureaaron.mod.utils.TextTransformer;
 import net.fabricmc.fabric.api.client.screen.v1.ScreenEvents;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.DrawContext;
@@ -33,21 +35,21 @@ import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
 
 public class ImagePreview {
-	private static final Pattern IMAGE_URL_PATTERN = Pattern.compile("(https?:\\/\\/.*\\.(?:png|jpg|jpeg|gif)\\.?(?:\\?.+)?)", Pattern.CASE_INSENSITIVE);
+	private static final Logger LOGGER = LogUtils.getLogger();
+	//https:\/\/.*\.(?:png|jpg|jpeg)\.?(?:\?.+)?
+	//TODO add tests for this regex
+	private static final Pattern IMAGE_URL_PATTERN = Pattern.compile("https:\\/\\/.*\\.(?:png|jpg|jpeg)\\.?(?:\\?.+)?", Pattern.CASE_INSENSITIVE);
 	private static final ImmutableSet<String> EXPECTED_CONTENT_TYPES = ImmutableSet.of("image/png", "image/jpeg");
-
 	private static final AtomicInteger COUNTER = new AtomicInteger();
-	//The actual image caches, we retain a separate set of urls to avoid attempting to cache the same image twice which'd cause a memory leak
-	private static final Object2ObjectOpenHashMap<String, CachedImage> IMAGE_CACHE = new Object2ObjectOpenHashMap<>();
-	private static final ObjectOpenHashSet<String> IMAGE_URLS_CACHED = new ObjectOpenHashSet<>();
+	private static final Map<String, CachedImage> IMAGE_CACHE = new ConcurrentHashMap<>();
 
 	@Init
 	public static void init() {
 		ReceiveChatMessageEvent.EVENT.register(ImagePreview::inspectMessageForImageLinks);
-		ScreenEvents.AFTER_INIT.register((client, screen, scaledWidth, scaledHeight) -> {
+		ScreenEvents.AFTER_INIT.register((client, screen, _scaledWidth, _scaledHeight) -> {
 			if (screen instanceof ChatScreen && AaronModConfigManager.get().imagePreview) {
-				ScreenEvents.afterRender(screen).register((screen1, context, mouseX, mouseY, delta) -> {
-					afterScreenRendered(screen1, context, mouseX, mouseY);
+				ScreenEvents.afterRender(screen).register((screen1, context, mouseX, mouseY, _delta) -> {
+					afterScreenRendered(client, screen1, context, mouseX, mouseY);
 				});
 			}
 		});
@@ -56,22 +58,22 @@ public class ImagePreview {
 	private static void inspectMessageForImageLinks(Text text, boolean overlay, boolean cancelled) {
 		if (AaronModConfigManager.get().imagePreview && !overlay && !cancelled) {
 			ObjectOpenHashSet<String> foundImages = new ObjectOpenHashSet<>();
-			Text deconstructedText = TextTransformer.deconstructAllComponents(text); 
-			List<Text> components = deconstructedText.getSiblings();
 
-			for (Text currentComponent : components) {
-				Style currentStyle = currentComponent.getStyle();
-				ClickEvent clickEvent = currentStyle.getClickEvent();
+			//Visit the text to find all image links inside of its click events
+			text.visit((style, stringified) -> {
+				ClickEvent clickEvent = style.getClickEvent();
 
 				if (clickEvent != null && clickEvent.getAction() == ClickEvent.Action.OPEN_URL) {
 					String url = fixupLink(clickEvent.getValue());
 
 					if (IMAGE_URL_PATTERN.matcher(url).matches()) foundImages.add(url);
 				}
-			}
 
-			for (String imageUrl: foundImages) {
-				if (!IMAGE_URLS_CACHED.contains(imageUrl)) {
+				return Optional.empty();
+			}, Style.EMPTY);
+
+			for (String imageUrl : foundImages) {
+				if (!IMAGE_CACHE.containsKey(imageUrl)) {
 					cacheImage(imageUrl);
 				}
 			}
@@ -80,31 +82,34 @@ public class ImagePreview {
 
 	private static void cacheImage(String url) {
 		CompletableFuture.runAsync(() -> {
-			MinecraftClient client = MinecraftClient.getInstance();
-			URI uri = URI.create(url);
+			try {
+				URI uri = URI.create(url);
 
-			if (isAllowedImageHost(uri.getHost())) {
-				IMAGE_URLS_CACHED.add(url);
+				if (isAllowedImageHost(uri.getHost())) {
+					//Insert a temporary value to ensure we don't try to cache this twice - ConcurrentHashMaps don't support nulls
+					IMAGE_CACHE.put(url, CachedImage.EMPTY);
 
-				try {
+					MinecraftClient client = MinecraftClient.getInstance();
 					InputStream inputStream = Http.sendGenericH2Request(uri, EXPECTED_CONTENT_TYPES);
 					NativeImage image = NativeImage.read(inputStream);
 					Identifier id = Identifier.of(Main.NAMESPACE, "image_preview/" + COUNTER.getAndIncrement());
 
-					client.getTextureManager().registerTexture(id, new NativeImageBackedTexture(image));
-
-					IMAGE_CACHE.put(url, new CachedImage(System.currentTimeMillis(), id, image.getWidth(), image.getHeight()));
-				} catch (Exception e) {
-					Main.LOGGER.error("[Aaron's Mod Image Preivew] Failed to cache image! URL: {}", url, e);
-
-					IMAGE_URLS_CACHED.remove(url);
+					//Schedule the image to be uploaded as a texture on the render thread to avoid Mojang's
+					//horribly dangerous system for handling off-thread GL operations
+					client.send(() -> {
+						client.getTextureManager().registerTexture(id, new NativeImageBackedTexture(image));
+						IMAGE_CACHE.put(url, new CachedImage(System.currentTimeMillis(), id, image.getWidth(), image.getHeight()));
+					});
 				}
+			} catch (Exception e) {
+				LOGGER.error("[Aaron's Mod Image Preivew] Failed to cache image! URL: {}", url, e);
 			}
 		});
 	}
 
-	private static void afterScreenRendered(Screen screen, DrawContext context, int mouseX, int mouseY) {
-		MinecraftClient client = MinecraftClient.getInstance();
+	private static void afterScreenRendered(MinecraftClient client, Screen screen, DrawContext context, int mouseX, int mouseY) {
+		if (!AaronModConfigManager.get().imagePreview) return;
+
 		Style style = client.inGameHud.getChatHud().getTextStyleAt(mouseX, mouseY);
 
 		if (style != null && style.getClickEvent() != null) {
@@ -113,13 +118,13 @@ public class ImagePreview {
 			if (clickEvent.getAction() == ClickEvent.Action.OPEN_URL) {
 				CachedImage image = ImagePreview.IMAGE_CACHE.getOrDefault(fixupLink(clickEvent.getValue()), null);
 
-				if (image != null) {
+				if (image != null && image != CachedImage.EMPTY) {
 					MatrixStack matrices = context.getMatrices();
 					int width = image.width();
 					int height = image.height();
 
-					//Reasonably scale the image so it doesn't go off screen and isn't too large or too small
-					float scale = Math.min(1f, Math.min(getPrevHeightDiv() / height, getPrevWidthDiv() / width));
+					//Scales the image to fit in a 16:9 ratio while ensuring that it can't get bigger
+					float scale = Math.min(1f, Math.min(getPrevWidthDiv() / width, getPrevHeightDiv() / height));
 
 					matrices.push();
 					matrices.scale(scale, scale, 1f); //The 1f is needed otherwise it'll render behind the chat (the chat's z is scaled by 1 too)
@@ -142,7 +147,6 @@ public class ImagePreview {
 		}
 
 		IMAGE_CACHE.clear();
-		IMAGE_URLS_CACHED.clear();
 	}
 
 	private static boolean isAllowedImageHost(String host) {
@@ -158,13 +162,15 @@ public class ImagePreview {
 		return url;
 	}
 
-	private static float getPrevHeightDiv() {
-		return (6.75f * 15f) * AaronModConfigManager.get().imagePreviewScale;
-	}
-
 	private static float getPrevWidthDiv() {
-		return (12f * 15f) * AaronModConfigManager.get().imagePreviewScale;
+		return (16 * 16f) * AaronModConfigManager.get().imagePreviewScale;
 	}
 
-	private static record CachedImage(long creationTime, Identifier texture, int width, int height) {}
+	private static float getPrevHeightDiv() {
+		return (9f * 16f) * AaronModConfigManager.get().imagePreviewScale;
+	}
+
+	private static record CachedImage(long creationTime, Identifier texture, int width, int height) {
+		private static final CachedImage EMPTY = new CachedImage(0, null, 0, 0);
+	}
 }
