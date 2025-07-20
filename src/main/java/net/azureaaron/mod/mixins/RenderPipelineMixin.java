6e9816f4aa3f5e05c4dec7049103bfc0e52ddaa6
Aaron <51387595+AzureAaron@users.noreply.github.com>
2025-07-20 15:14:21 -0400

    Separate Aaron Mod's glow from vanilla's glow

--- src/main/java/net/azureaaron/mod/mixins/RenderPipelineMixin.java
+++ src/main/java/net/azureaaron/mod/mixins/RenderPipelineMixin.java
@@ -7,8 +7,7 @@ import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 import com.mojang.blaze3d.pipeline.RenderPipeline;
 import com.mojang.blaze3d.platform.DepthTestFunction;
 
-import net.azureaaron.mod.config.AaronModConfigManager;
-import net.azureaaron.mod.utils.Cache;
+import net.azureaaron.mod.utils.render.GlowRenderer;
 import net.minecraft.client.gl.RenderPipelines;
 
 @Mixin(RenderPipeline.class)
@@ -16,6 +15,6 @@ public class RenderPipelineMixin {
 
 	@ModifyReturnValue(method = "getDepthTestFunction", at = @At("RETURN"))
 	private DepthTestFunction aaronMod$modifyGlowDepthTest(DepthTestFunction original) {
-		return ((Object) this == RenderPipelines.OUTLINE_CULL || (Object) this == RenderPipelines.OUTLINE_NO_CULL) && Cache.inM7Phase5 && AaronModConfigManager.get().skyblock.m7.glowingDragons ? DepthTestFunction.LEQUAL_DEPTH_TEST : original;
+		return ((Object) this == RenderPipelines.OUTLINE_CULL || (Object) this == RenderPipelines.OUTLINE_NO_CULL) && GlowRenderer.isRenderingGlow() ? DepthTestFunction.LEQUAL_DEPTH_TEST : original;
 	}
 }
--- src/main/java/net/azureaaron/mod/mixins/WorldRendererMixin.java
+++ src/main/java/net/azureaaron/mod/mixins/WorldRendererMixin.java
@@ -9,43 +9,41 @@ import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.Slice;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import com.llamalad7.mixinextras.sugar.Local;
-import com.llamalad7.mixinextras.sugar.Share;
-import com.llamalad7.mixinextras.sugar.ref.LocalBooleanRef;
 import com.mojang.blaze3d.buffers.GpuBufferSlice;
 
 import net.azureaaron.mod.config.AaronModConfigManager;
 import net.azureaaron.mod.mixins.accessors.FogRendererAccessor;
 import net.azureaaron.mod.mixins.accessors.GameRendererAccessor;
 import net.azureaaron.mod.utils.Cache;
+import net.azureaaron.mod.utils.render.GlowRenderer;
 import net.minecraft.block.enums.CameraSubmersionType;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.render.Camera;
-import net.minecraft.client.render.DefaultFramebufferSet;
+import net.minecraft.client.render.OutlineVertexConsumerProvider;
 import net.minecraft.client.render.WorldRenderer;
 import net.minecraft.client.render.fog.FogModifier;
 import net.minecraft.client.render.fog.FogRenderer;
 import net.minecraft.client.render.fog.FogRenderer.FogType;
 import net.minecraft.client.render.fog.StatusEffectFogModifier;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.boss.dragon.EnderDragonEntity;
 
 @Mixin(WorldRenderer.class)
 public class WorldRendererMixin {
 	@Shadow
 	@Final
 	private MinecraftClient client;
-	@Shadow
-	@Final
-	private DefaultFramebufferSet framebufferSet;
 
-	@Inject(method = "method_62214",
-			slice = @Slice(from = @At(value = "INVOKE", target = "Lnet/minecraft/client/render/WorldRenderer;canDrawEntityOutlines()Z")),
-			at = @At(value = "INVOKE", target = "Lcom/mojang/blaze3d/systems/CommandEncoder;clearColorAndDepthTextures(Lcom/mojang/blaze3d/textures/GpuTexture;ILcom/mojang/blaze3d/textures/GpuTexture;D)V", ordinal = 0, shift = At.Shift.AFTER)
-	)
-	private void aaronMod$copyFramebufferDepth2AdjustGlowVisibility(CallbackInfo ci, @Share(namespace = "c", value = "copiedOutlineDepth") LocalBooleanRef copiedOutlineDepth) {
-		if (Cache.inM7Phase5 && AaronModConfigManager.get().skyblock.m7.glowingDragons && !copiedOutlineDepth.get()) {
-			framebufferSet.entityOutlineFramebuffer.get().copyDepthFrom(framebufferSet.mainFramebuffer.get());
-			copiedOutlineDepth.set(true);
-		}
+	@ModifyExpressionValue(method = "renderEntities", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/render/BufferBuilderStorage;getOutlineVertexConsumers()Lnet/minecraft/client/render/OutlineVertexConsumerProvider;"))
+	private OutlineVertexConsumerProvider aaronMod$useCustomGlowOutlineVertexConsumers(OutlineVertexConsumerProvider original, @Local Entity entity) {
+		return entity instanceof EnderDragonEntity && Cache.inM7Phase5 && AaronModConfigManager.get().skyblock.m7.glowingDragons ? GlowRenderer.GLOW_OUTLINE_VERTEX_CONSUMERS : original;
+	}
+
+	@Inject(method = "method_62214", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/render/OutlineVertexConsumerProvider;draw()V"))
+	private void aaronMod$drawGlowVertexConsumers(CallbackInfo ci) {
+		GlowRenderer.GLOW_OUTLINE_VERTEX_CONSUMERS.draw();
 	}
 
 	@ModifyVariable(method = "render",
