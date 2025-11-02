f1b4c7862bc0817cf3703363e378e31113da0a04
Aaron <51387595+AzureAaron@users.noreply.github.com>
2025-11-02 12:41:42 -0500

    Fix Skyblocker Name Customization Chroma integration

--- src/main/java/net/azureaaron/mod/mixins/SkyblockerCustomizeNameWidgetMixin.java
+++ src/main/java/net/azureaaron/mod/mixins/SkyblockerCustomizeNameWidgetMixin.java
@@ -1,8 +1,5 @@
 package net.azureaaron.mod.mixins;
 
-import java.util.Arrays;
-
-import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Pseudo;
@@ -11,39 +8,36 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import com.google.common.collect.ImmutableList;
+import com.llamalad7.mixinextras.sugar.Local;
+
 import net.azureaaron.mod.features.ChromaText;
 import net.azureaaron.mod.utils.render.AaronModRenderPipelines;
 import net.minecraft.client.font.TextRenderer;
 import net.minecraft.client.gui.DrawContext;
-import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.screen.narration.NarrationMessageBuilder;
 import net.minecraft.client.gui.tooltip.Tooltip;
+import net.minecraft.client.gui.widget.ClickableWidget;
 import net.minecraft.client.gui.widget.GridWidget;
 import net.minecraft.client.gui.widget.PressableWidget;
 import net.minecraft.client.input.AbstractInput;
 import net.minecraft.text.Style;
 import net.minecraft.text.Text;
-import net.minecraft.util.Formatting;
 
-@Mixin(targets = "de.hysky.skyblocker.skyblock.item.custom.screen.name.CustomizeNameScreen")
+@Mixin(targets = "de.hysky.skyblocker.skyblock.item.custom.screen.name.CustomizeNameWidget")
 @Pseudo
-public abstract class SkyblockerCustomizeNameScreenMixin extends Screen {
+public abstract class SkyblockerCustomizeNameWidgetMixin {
 	@Shadow
 	@Final
 	private GridWidget grid;
 
-	protected SkyblockerCustomizeNameScreenMixin(Text title) {
-		super(title);
-	}
-
 	@Shadow
-	public abstract void setStyle(Style style);
+	protected abstract void setStyle(Style style);
 
-	@Inject(method = { "init", "method_25426" }, at = @At(value = "FIELD", target = "Lde/hysky/skyblocker/skyblock/item/custom/screen/name/CustomizeNameScreen;$assertionsDisabled:Z", opcode = Opcodes.GETSTATIC, shift = At.Shift.BEFORE), require = 0)
-	private void aaronMod$addChromaButton(CallbackInfo ci) {
+	@Inject(method = "addFormattingButtons", at = @At("RETURN"), require = 0)
+	private void aaronMod$addChromaButton(ImmutableList.Builder<ClickableWidget> builder, CallbackInfo ci, @Local(name = "colorButtonIndex") int colourButtonIndex) {
 		if (ChromaText.chromaColourAvailable()) {
-			int column = (int) Arrays.stream(Formatting.values()).filter(Formatting::isColor).count();
-			this.addDrawableChild(this.grid.add(new ChromaColourButton(), 2, column));
+			builder.add(grid.add(new ChromaColourButton(), 2, colourButtonIndex));
 		}
 	}
 
@@ -51,12 +45,12 @@ public abstract class SkyblockerCustomizeNameScreenMixin extends Screen {
 
 		public ChromaColourButton() {
 			super(0, 0, 16, 16, Text.literal("Chroma"));
-			this.setTooltip(Tooltip.of(Text.literal("Chroma")));
+			this.setTooltip(Tooltip.of(getMessage()));
 		}
 
 		@Override
 		public void onPress(AbstractInput input) {
-			SkyblockerCustomizeNameScreenMixin.this.setStyle(Style.EMPTY.withColor(0xAA5500));
+			SkyblockerCustomizeNameWidgetMixin.this.setStyle(Style.EMPTY.withColor(0xAA5500));
 		}
 
 		@Override
