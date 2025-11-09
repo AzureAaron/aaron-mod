1d400b22901a85fe194db6d234acc54cc47931e1
Aaron <51387595+AzureAaron@users.noreply.github.com>
2025-11-09 14:27:06 -0500

    Adjust hiding of level loading screens

--- src/main/java/net/azureaaron/mod/mixins/LevelLoadingScreenMixin.java
+++ src/main/java/net/azureaaron/mod/mixins/LevelLoadingScreenMixin.java
@@ -1,18 +1,22 @@
 package net.azureaaron.mod.mixins;
 
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import net.azureaaron.mod.config.AaronModConfigManager;
 import net.minecraft.client.gui.screen.world.LevelLoadingScreen;
+import net.minecraft.client.world.ClientChunkLoadProgress;
 
 @Mixin(LevelLoadingScreen.class)
 public class LevelLoadingScreenMixin {
+	@Shadow
+	private ClientChunkLoadProgress chunkLoadProgress;
 
 	@Inject(method = { "render", "renderBackground" }, at = @At("HEAD"), cancellable = true)
 	private void aaronMod$hideWorldLoadingScreen(CallbackInfo ci) {
-		if (AaronModConfigManager.get().uiAndVisuals.world.hideWorldLoadingScreen) ci.cancel();
+		if (AaronModConfigManager.get().uiAndVisuals.world.hideWorldLoadingScreen && this.chunkLoadProgress.getChunkLoadMap() == null) ci.cancel();
 	}
 }
