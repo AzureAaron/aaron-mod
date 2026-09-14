0cbd377613242fdf691585db7c701000bbc9da46
Aaron <51387595+AzureAaron@users.noreply.github.com>
2026-09-14 01:17:46 -0400

    Use separate chroma text vertex shader

--- src/main/java/net/azureaaron/mod/utils/render/AaronModRenderPipelines.java
+++ src/main/java/net/azureaaron/mod/utils/render/AaronModRenderPipelines.java
@@ -15,7 +15,7 @@ import net.minecraft.client.renderer.RenderPipelines;
 
 public class AaronModRenderPipelines {
 	private static final RenderPipeline.Snippet CHROMA_TEXT_SNIPPET = RenderPipeline.builder()
-			.withVertexShader("core/text")
+			.withVertexShader(Main.id("core/chroma_text"))
 			.withFragmentShader(Main.id("core/chroma_text"))
 			.withShaderDefine("AARON_MOD_CHROMA")
 			.withBindGroupLayout(AaronModBindGroupLayouts.CHROMA)
